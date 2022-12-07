dd7267fdad33a2a2e1d71456ccdbe6d3323849ff
MarlenAndrade <93796896+MarlenAndrade@users.noreply.github.com>
2022-12-07 13:08:40 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Container from './components/Container';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Container/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
